0bbf5d9df4f585d86721929595e66c9779008f42
coldase <j.vihiniemi@gmail.com>
2022-04-06 14:07:28 +0300

    footer info

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,6 +4,29 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <img src={YritysSaloLogo} />
+
+      <div className="footer-info-container">
+        <div className="footer-info-grp">
+          <p>Joensuunkatu 7</p>
+          <p>24100 SALO</p>
+          <p>+358 44 778 2142</p>
+          <p>Yrityssalo@yrityssalo.fi</p>
+        </div>
+        <div className="footer-info-grp">
+          <p>toihinsaloon.fi</p>
+          <p>investinsalo.fi</p>
+          <p>
+            <nobr>Vapaat toimitilat</nobr>
+          </p>
+          <p>visitsalo.fi</p>
+        </div>
+        <div className="footer-info-grp">
+          <a href="https://twitter.com">Twitter</a>
+          <a href="https://linkedin.com">LindedIn</a>
+          <a href="https://facebook.com">Facebook</a>
+          <a href="https://instagram.com">Instagram</a>
+        </div>
+      </div>
     </div>
   );
 };
